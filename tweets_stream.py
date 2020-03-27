cd00f5fcb5099561ae679d5194489ef24364adcd
Antonio Cecere <antocecere77@gmail.com>
2020-03-27 06:55:29 +0100

    Custom datasource with socket

--- tweets_stream.py
+++ tweets_stream.py
@@ -1,7 +1,12 @@
+import socket
 import json
 import requests
 import requests_oauthlib
 
+CONSUMER_KEY = 'TWITTER_API_KEY'
+CONSUMER_SECRET = 'WITTER_API SECRET_KEY'
+ACCESS_TOKEN = 'TWITTER ACCESS TOKEN'
+ACCESS_SECRET = 'TWITTER ACCESS TOKEN SECRET'
 
 
 URL = 'https://stream.twitter.com/1.1/statuses/filter.json'
@@ -14,13 +19,43 @@ def get_tweets(topic):
     
     return response
 
-response = get_tweets("salvini")
+# response = get_tweets("salvini")
 
-for line in response.iter_lines():    
-    full_tweets = json.loads(line)
-    tweet = full_tweets["text"]
-    print(tweet)
-    print("-----------------------------------")
+#for line in response.iter_lines():    
+#    full_tweets = json.loads(line)
+#    tweet = full_tweets["text"]
+#    print(tweet)
+#    print("-----------------------------------")
+
+def send_tweets(response, conn):
+    for line in response.iter_lines():
+        try:
+            full_tweet = json.loads(line)
+            tweet = full_tweet["text"]
+            print("Tweet:" + tweet)
+            print("-----------------------------------")
+            conn.send((tweet + "\n").encode())
+        except Exception as e:
+            print(e)
+    
+topic = input("Cosa vuoi monitorare? ").lower()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 9999))
+s.listen(1)
+
+print("In attesa di una connessione...")
+
+conn, addr = s.accept()
+
+print("Connesso! In attesa di tweets...")
+
+response = get_tweets(topic)
+send_tweets(response, conn)
+
+
+
     
  
+
     
